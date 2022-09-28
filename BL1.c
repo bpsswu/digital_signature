334a3b150516941449f4b6cbe47ef7dfa4f77d3f
백승욱 <baekseungwook@SWmacbook.local>
2022-09-28 21:41:44 +0900

    Enc, Dec Success

--- BL1.c
+++ BL1.c
@@ -28,18 +28,13 @@ int main()
     char* file_contents = malloc(sb.st_size); // st_size = total size in byte
     fread(file_contents, sb.st_size, 1, in_file);
 
-    // // SHA-256 시작
+    // SHA-256 시작
     // unsigned char digest[SHA256_DIGEST_LENGTH]; // SHA256_DIGEST_LENGTH = 32
-    // /*
-    //     unsigned char *SHA256(const unsigned char *d, size_t n, unsigned char *md);
-    // */
+    /*
+        unsigned char *SHA256(const unsigned char *d, size_t n, unsigned char *md);
+    */
     // SHA256(file_contents, sb.st_size, digest);
 
-    // // printf("입력값 : ");
-    // // for (int i = 0; i < sb.st_size; i++)
-    // // {
-    // //     printf("%x", file_contents[i]);
-    // // }
     // printf("\n입력 파일 크기 : %lld", sb.st_size);
     // printf("\n");
 
@@ -66,29 +61,49 @@ int main()
     printf("\n");
 
     // RSA 시작
-    unsigned char cipher_text[256]; // 암호화된 데이터가 들어갈 배열
-    unsigned char plain_text_receiver[256]; // cipher_text를 복호화한 데이터
+    unsigned char cipher_text[256];
+    unsigned char plain_text[256];
 
     // RSA *PEM_read_RSAPrivateKey(FILE *fp, RSA **x, pem_password_cb *cb, void *u);
     // 마지막 두 인수는 파일에 비밀번호가 걸려있을 때만 해당
     FILE *privfp = fopen("private_key.pem", "r");
     RSA *rsa_pri = PEM_read_RSAPrivateKey(privfp, NULL, NULL, NULL);
 
-    // FILE *pubfp = fopen("public_key.pem", "r");
-    // RSA *rsa_pub = PEM_read_RSA_PUBKEY(pubfp, NULL, NULL, NULL);
+    const BIGNUM *n = BN_new();
+    const BIGNUM *e = BN_new();
+    const BIGNUM *d = BN_new();
+    char * n_str = NULL;
+    char * e_str = NULL;
+    char * d_str = NULL;
+
+    RSA_get0_key(rsa_pri, &n, &e, &d);
+
+    // printf("\n");
+    // n_str = BN_bn2hex(n);
+    // printf("[DEBUG] n_str = %s\n", n_str);
+    // e_str = BN_bn2hex(e);
+    // printf("[DEBUG] e_str = %s\n", e_str);
+    // d_str = BN_bn2hex(d);
+    // printf("[DEBUG] d_str = %s\n", d_str);
+
+    FILE *pubfp = fopen("public_key.pem", "r");
+    RSA *rsa_pub = PEM_read_RSA_PUBKEY(pubfp, NULL, NULL, NULL);
+    //RSA *rsa_pub = PEM_read_RSAPublicKey(pubfp, NULL, NULL, NULL);
+
+    // RSA_get0_key(rsa_pub, &n, &e, &d);
+    // printf("\n");
+    // n_str = BN_bn2hex(n);
+    // printf("[DEBUG] n_str = %s\n", n_str);
+    // e_str = BN_bn2hex(e);
+    // printf("[DEBUG] e_str = %s\n", e_str);
 
     // 키 유효성 검사
     if(RSA_check_key(rsa_pri) != 1)
     {
+        // 아 RSA_check_key 함수는 개인키 전용 함수였음
         printf("유효하지 않은 RSA Private 키\n");
         return 0;
     }
-
-    // if(RSA_check_key(rsa_pub) != 1)
-    // {
-    //     printf("유효하지 않은 RSA Public 키\n");
-    //     return 0;
-    // }
 
     /*
     ※ 주의 ※
@@ -102,22 +117,23 @@ int main()
     printf("[DEBUG] sizeof(digest) = %lu\n", sizeof(hash_value));
     printf("[DEBUG] RSA_size(rsa_pri) = %d\n", RSA_size(rsa_pri));
 
+    // 자체 패딩
     unsigned char digest_padded[256] = {0, };
-
     for (int k = 0; k < 32; k++)
     {
         digest_padded[k+4] = hash_value[k];
     }
 
-    printf("\n패딩 결과 :\n");
+    printf("\n패딩된 해시값 :\n");
     for (int j = 0; j < 256; j++)
     {
         printf("%02X ", digest_padded[j]);
     }
     printf("\n");
 
-    // 암호화 과정(개인키)
-    RSA_private_encrypt(RSA_size(rsa_pri), digest_padded, cipher_text, rsa_pri, RSA_NO_PADDING);
+    // 암호화 과정 (개인키)
+    unsigned int num;
+    num = RSA_private_encrypt(RSA_size(rsa_pri), digest_padded, cipher_text, rsa_pri, RSA_NO_PADDING);
     printf("\n해시값 -> 암호문 : \n");
     for (int i = 0; i < 256; i++)
     {
@@ -125,18 +141,15 @@ int main()
     }
     printf("\n");
 
-    // // 복호화 과정(공개키)
-    // num = RSA_public_decrypt(num, cipher_text, plain_text_receiver, key_pair, RSA_PKCS1_PADDING);
-
-    // printf("\n\n[DEBUG] num = %d\n\n", num);
-
-    // plain_text_receiver[num] = '\0';
-    // printf("\n복호화된 데이터 : ");
-    // for (int i = 0; plain_text_receiver[i] != '\0'; i++)
-    // {
-    //     printf("%x", plain_text_receiver[i]);
-    // }
-    // printf("\n");
+    // 복호화 과정 (공개키)
+    num = RSA_public_decrypt(num, cipher_text, plain_text, rsa_pub, RSA_NO_PADDING);
+    plain_text[num] = '\0';
+    printf("\n암호문 -> 해시값 : \n");
+    for (int i = 0; i < 256; i++)
+    {
+        printf("%02X ", plain_text[i]);
+    }
+    printf("\n");
 
     printf("\n...프로그램 종료...\n\n");
     fclose(in_file);
