aa2ad1e397b263333b2cf71dbe1a7026d6ffcb57
bpsswu <bpsswu@gmail.com>
2022-09-28 15:26:21 +0900

    Delete some codes

--- BL1.c
+++ BL1.c
@@ -69,36 +69,6 @@ int main()
     unsigned char cipher_text[256]; // 암호화된 데이터가 들어갈 배열
     unsigned char plain_text_receiver[256]; // cipher_text를 복호화한 데이터
 
-    // // BIGNUM 구조체 생성
-    // BIGNUM *bne = BN_new();
-    // if(bne == NULL)
-    // {
-    //     printf("BIGNUM 구조체 생성 실패\n");
-    //     return 0;
-    // }
-
-    // // BIGNUM 구조체 값 할당
-    // if(BN_set_word(bne, RSA_F4) != 1)
-    // {
-    //     printf("BIGNUM 구조체에 값 할당 실패\n");
-    //     return 0;
-    // }
-
-    // // RSA 구조체 생성
-    // RSA *key_pair = RSA_new();
-    // if(key_pair == NULL)
-    // {
-    //     printf("RSA 구조체 생성 실패\n");
-    //     return 0;
-    // }
-
-    // // 공용키, 개인키 생성
-    // if(RSA_generate_key_ex(key_pair, 2048, bne, NULL) != 1)
-    // {
-    //     printf("RSA 키 생성 실패\n");
-    //     return 0;
-    // }
-
     // RSA *PEM_read_RSAPrivateKey(FILE *fp, RSA **x, pem_password_cb *cb, void *u);
     // 마지막 두 인수는 파일에 비밀번호가 걸려있을 때만 해당
     FILE *privfp = fopen("private_key.pem", "r");
@@ -139,23 +109,15 @@ int main()
         digest_padded[k+4] = hash_value[k];
     }
 
-    printf("\n");
+    printf("\n패딩 결과 :\n");
     for (int j = 0; j < 256; j++)
     {
         printf("%02X ", digest_padded[j]);
     }
     printf("\n");
 
-    unsigned int num;
-    // num = RSA_public_encrypt(sizeof(hash_value), hash_value, cipher_text, rsa_pri, RSA_NO_PADDING);
-    // printf("\n해시값 -> 암호문 : ");
-    // for (int i = 0; i < 256; i++)
-    // {
-    //     printf("%02X ", cipher_text[i]);
-    // }
-    printf("\n");
-
-    num = RSA_public_encrypt(RSA_size(rsa_pri), digest_padded, cipher_text, rsa_pri, RSA_NO_PADDING);
+    // 암호화 과정(개인키)
+    RSA_private_encrypt(RSA_size(rsa_pri), digest_padded, cipher_text, rsa_pri, RSA_NO_PADDING);
     printf("\n해시값 -> 암호문 : \n");
     for (int i = 0; i < 256; i++)
     {
@@ -163,8 +125,8 @@ int main()
     }
     printf("\n");
 
-    // // 복호화 과정
-    // num = RSA_private_decrypt(num, cipher_text, plain_text_receiver, key_pair, RSA_PKCS1_PADDING);
+    // // 복호화 과정(공개키)
+    // num = RSA_public_decrypt(num, cipher_text, plain_text_receiver, key_pair, RSA_PKCS1_PADDING);
 
     // printf("\n\n[DEBUG] num = %d\n\n", num);
 
@@ -177,9 +139,6 @@ int main()
     // printf("\n");
 
     printf("\n...프로그램 종료...\n\n");
-    // 구조체 메모리 해제
-    // RSA_free(key_pair);
-    // BN_free(bne);    
     fclose(in_file);
     free(file_contents);
     exit(EXIT_SUCCESS);
